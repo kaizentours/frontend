073a6806ac8267d1c2f8a233d7440f066259b458
kaizentours <kiran7756@yahoo.com>
2026-03-24 21:43:49 -0500

    Add dist build

--- src/app/services/website.service.ts
+++ src/app/services/website.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({ providedIn: 'root' })
 export class WebsiteService {
-  private apiUrl = 'http://localhost:3000/api';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
